e66294bdbd51c6bed81ec8725cf8635a8a447588
Lance Desk <lncedesk@gmail.com>
2023-10-03 06:18:46 -0700

    A program that prints all possible different combinations of two digits.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,6 @@
 # Print all possible different combinations of two digits
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:02d}".format(i * 10 + j), end=", " if i != 8 or (i == 8 and j != 9) else "\n")
+        combination = i * 10 + j
+        ending = ", " if i != 8 or (i == 8 and j != 9) else "\n"
+        print("{:02d}".format(combination), end=ending)
