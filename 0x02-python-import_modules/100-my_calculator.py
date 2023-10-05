e8fbbfaefb47a3259ae8ddb5b2910d2d80a4a7d2
Lance Desk <lncedesk@gmail.com>
2023-10-05 10:00:49 -0700

    A program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,26 +3,25 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
-arg_no = len(sys.argv)
-program_name = sys.argv[0]
-
-if (arg_no == 4):
-    operator = sys.argv[2][0]
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-
 if __name__ == "__main__":
-    if (arg_no != 4):
+    arg_no = len(sys.argv)
+    program_name = sys.argv[0]
+
+    if arg_no != 4:
         print("Usage: {} <a> <operator> <b>".format(program_name))
         sys.exit(1)
-    elif(arg_no == 4):
-        if(operator == '+'):
+    else:
+        operator = sys.argv[2][0]
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+
+        if operator == '+':
             print("{:d} {} {:d} = {:d}".format(a, operator, b, add(a, b)))
-        elif(operator == '-'):
+        elif operator == '-':
             print("{:d} {} {:d} = {:d}".format(a, operator, b, sub(a, b)))
-        elif(operator == '*'):
+        elif operator == '*':
             print("{:d} {} {:d} = {:d}".format(a, operator, b, mul(a, b)))
-        elif(operator == '/'):
+        elif operator == '/':
             print("{:d} {} {:d} = {:d}".format(a, operator, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
