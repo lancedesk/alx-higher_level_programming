090af9f17ab87917f9d2906d3e3b627dbb095a4f
Lance Desk <lncedesk@gmail.com>
2023-10-06 23:19:40 -0700

    A function that prints a matrix of integers.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,11 @@
 
 def print_matrix_integer(matrix=[[]]):
     if not matrix or not matrix[0]:
-        print("{}".format(""))
+        print("")  # Print an empty line if matrix is empty or has empty rows
     else:
         for row in matrix:
-            for column in row:
-                print("{:d}".format(column), end=" ")
-            print()
+            for index, column in enumerate(row):
+                print("{:d}".format(column), end="")
+                if index != len(row) - 1:
+                    print(" ", end="")  # Add space between numbers in same row
+            print("")  # Move to the next line for the next row
