b32601a324b01292797463702c03de4b3449c1f9
Lance Desk <lncedesk@gmail.com>
2023-10-23 11:12:37 -0700

    A function that executes a function safely.

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+
+    try:
+        return (fct(*args))
+
+    except Exception as e:
+        stderr.write("Exception: {}\n".format(e))
+        return (None)
