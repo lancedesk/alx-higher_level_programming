49b136639ca8dd304e3a7519bed3e79104821d37
Lance Desk <lncedesk@gmail.com>
2023-10-29 03:51:20 -0700

    A function that adds 2 integers.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,26 +4,18 @@ def add_integer(a, b=98):
     """
     Adds two integers.
 
-    Args:
-        a (int or float): First integer.
-        b (int or float, optional): Second integer (default is 98).
-
-    Returns:
-        int: The result of the addition.
-
-    Raises:
-        TypeError: If a or b is not an integer or float.
+    Prototype: def add_integer(a, b=98):
+    a and b must be integers or floats, otherwise raise a TypeError exception
+    with the message a must be an integer or b must be an integer
+    a and b must be first casted to integers if they are float
+    Returns an integer: the addition of a and b
     """
-    # Check if a and b are integers or floats
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
 
-    # Cast floats to integers
     a = int(a)
     b = int(b)
 
-    # Perform the addition
-    result = a + b
-
-    # Return the result
-    return (result)
+    return (a + b)
