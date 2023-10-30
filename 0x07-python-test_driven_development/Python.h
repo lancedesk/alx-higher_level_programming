af77ecd3d3989bf33dc17aba2dbee9ad80f7b67d
Lance Desk <lncedesk@gmail.com>
2023-10-30 05:31:15 -0700

    A function that prints Python strings

---
+++ 0x07-python-test_driven_development/Python.h
@@ -0,0 +1,10 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+
+#include <stdio.h>   /* Standard I/O functions */
+#include <stdlib.h>  /* Standard library functions */
+#include <string.h>  /* String manipulation functions */
+
+void print_python_string(PyObject *p);
+
+#endif
