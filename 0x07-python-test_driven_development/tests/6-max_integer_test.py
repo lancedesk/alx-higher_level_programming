5557059ce7a2459d67e1b58d50cedd4d06f59b3f
Lance Desk <lncedesk@gmail.com>
2023-10-30 04:51:29 -0700

    unittests for the function def max_integer(list=[]):

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,119 +1,54 @@
 #!/usr/bin/python3
-"""Unittest with max_integer([..])
-"""
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+    """Test cases for the max_integer function."""
+
     def test_empty_list(self):
-        """
-        Tests with empty lists
-
-        Args:
-            self : Argument
-
-        """
+        """Test when list is empty."""
         self.assertIsNone(max_integer([]))
 
     def test_one_element(self):
-        """
-        Tests with 1 element
-
-        Args:
-            self : Argument
-
-        """
+        """Test when list contains only one element."""
         self.assertEqual(max_integer([5]), 5)
 
     def test_2_elements(self):
-        """
-        Tests with 2 elements
-
-        Args:
-            self : Argument
-
-        """
+        """Test when list contains two elements."""
         self.assertEqual(max_integer([5, 2]), 5)
 
     def test_all_neg_elements(self):
-        """
-        Tests with all negative elements without 0
-
-        Args:
-            self : Argument
-
-        """
+        """Test when list contains all negative elements."""
         self.assertEqual(max_integer([-5, -3, -1, -4, -2]), -1)
 
     def test_all_neg_with_0(self):
-        """
-        Tests with all negative elements plus 0
-
-        Args:
-            self : Argument
-
-        """
+        """Test when list contains all negative elements plus 0."""
         self.assertEqual(max_integer([-5, -3, -1, -4, 0, -2]), 0)
 
     def test_mixed_elements(self):
-        """
-        Tests with mixed signed elements
-
-        Args:
-            self : Argument
-
-        """
+        """Test when list contains mixed positive and negative elements."""
         self.assertEqual(max_integer([-1, 2, -3, 4, -5]), 4)
 
     def test_same_elements_not_max(self):
-        """
-        Tests with multiples that aren't max
-
-        Args:
-            self : Argument
-
-        """
+        """Test when list contains multiples that aren't max."""
         self.assertEqual(max_integer([2, 4, 6, 4, 8]), 8)
 
     def test_same_elements_max(self):
-        """
-        Tests with multiples that are max
-
-        Args:
-            self : Argument
-
-        """
+        """Test when list contains multiples that are max."""
         self.assertEqual(max_integer([8, 4, 6, 8, 8]), 8)
 
     def test_all_0(self):
-        """
-        Tests with all 0
-
-        Args:
-            self : Argument
-
-        """
+        """Test when list contains all 0."""
         self.assertEqual(max_integer([0, 0, 0, 0, 0]), 0)
 
     def test_large_numbers(self):
-        """
-        Tests with large numbers
-
-        Args:
-            self : Argument
-
-        """
+        """Test when list contains large numbers."""
         self.assertEqual(max_integer([1000000, 5000000, 3000000]), 5000000)
 
     def test_many_elements(self):
-        """
-        Tests with many elements
-
-        Args:
-            self : Argument
-
-        """
+        """Test when list contains many elements."""
         self.assertEqual(max_integer([-50, 0, 10, -20, 30, -40, 0, 5, 15,
                                       -25, 35, -45, 0, 25, -15, 20, -30, 40,
                                       -10, 50, 0, -5, 45, -35, 30, 0, -20, 10,
@@ -130,5 +65,4 @@ class TestMaxInteger(unittest.TestCase):
 
 
 if __name__ == '__main__':
-
     unittest.main()
