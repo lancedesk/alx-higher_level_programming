c31309cdf44fd25d903ea239899eb06291bed5e9
Lance Desk <lncedesk@gmail.com>
2023-10-31 04:39:17 -0700

    A class LockedClass with no class or object attribute, that prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called first_name.

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+""" Define a class called LockedClass."""
 
 
-# Define a class called LockedClass.
 class LockedClass:
-    # Define a special attribute __slots__
-    # which restricts the creation of new instance attributes
-    # except for 'first_name'.
+    """
+    Define a special attribute __slots__
+    Which restricts the creation of new instance attributes
+    except for 'first_name'.
+    """
+
     __slots__ = ['first_name']
