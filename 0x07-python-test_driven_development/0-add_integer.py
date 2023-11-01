a6a16464652cae5ab0208407a0f26501747ddc7a
Lance Desk <lncedesk@gmail.com>
2023-11-01 05:00:38 -0700

    Add module description

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+This module provides a function to add two integers.
+
+Prototype: def add_integer(a, b=98):
+a and b must be integers or floats, otherwise raise a TypeError exception
+with the message a must be an integer or b must be an integer
+a and b must be first casted to integers if they are float
+Returns an integer: the addition of a and b
+"""
+
 
 def add_integer(a, b=98):
     """
