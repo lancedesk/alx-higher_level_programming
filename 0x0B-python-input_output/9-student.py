b040d8c01eb72134e99d5f1eacdcbe29d763bd38
Lance Desk <lncedesk@gmail.com>
2023-11-07 14:31:12 -0800

    A class Student that defines a student by:

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module to define a Student class.
+"""
+
+class Student:
+    """
+    Class representing a student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with first_name, last_name, and age.
+
+        Args:
+            first_name (str): First name of the student.
+            last_name (str): Last name of the student.
+            age (int): Age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: Dictionary representation of the Student instance.
+        """
+        return self.__dict__
