955bb005eee230dde87ec1e675d5e107ebcd2bef
Lance Desk <lncedesk@gmail.com>
2023-11-07 14:38:36 -0800

    A class Student that defines a student by: (based on 9-student.py)

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Module to define a Student class.
+"""
+
+
+class Student:
+    """
+    Class representing a student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with first_name, last_name, and age.
+
+        Args:
+            first_name (str): First name of the student.
+            last_name (str): Last name of the student.
+            age (int): Age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list): List of attribute names to be retrieved
+            (default is None).
+
+        Returns:
+            dict: Dictionary representation of the Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            filtered_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    filtered_dict[attr] = getattr(self, attr)
+            return filtered_dict
