b9de85ef6868283752b513e7073182c7ae90864a
Lance Desk <lncedesk@gmail.com>
2023-11-11 05:47:30 -0800

    JSON string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """Module containing the Base class for managing id attribute."""
 import json
 
+
 class Base:
     """Base class for managing id attribute."""
 
@@ -28,4 +29,29 @@ class Base:
         """Return the JSON string representation of list_dictionaries."""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        return json.dumps(list_dictionaries, sort_keys=True)
+        # Sort the list of dictionaries based on the 'id' key
+        sorted_list = sorted(list_dictionaries, key=lambda x: x.get('id', 0))
+        return json.dumps(sorted_list)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save the JSON string representation of list_objs to a file."""
+        if list_objs is None:
+            list_objs = []
+
+        # Get the class name (assuming all elements in list_objs
+        # are of the same class)
+        class_name = cls.__name__
+
+        # Create the filename
+        filename = "{}.json".format(class_name)
+
+        # Convert instances to a list of dictionaries
+        dict_list = [obj.to_dictionary() for obj in list_objs]
+
+        # Convert the list of dictionaries to a JSON string
+        json_string = cls.to_json_string(dict_list)
+
+        # Write the JSON string to the file
+        with open(filename, 'w') as file:
+            file.write(json_string)
