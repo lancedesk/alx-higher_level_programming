e85cf7f07f63f6c904a98457990182816fef8735
Lance Desk <lncedesk@gmail.com>
2023-12-14 06:10:47 -0800

    A script that creates the table id_not_null on your MySQL server.

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,9 +1,5 @@
 -- Create the table id_not_null
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT DEFAULT 1,
-    name VARCHAR(256),
-    PRIMARY KEY (id)
-) ENGINE=InnoDB;
-
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
+    name VARCHAR(256)
+);
