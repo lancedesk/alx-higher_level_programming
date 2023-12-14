9c894da65cf9d53504d2b1b7df55c5d22e1b6f97
Lance Desk <lncedesk@gmail.com>
2023-12-14 00:39:37 -0800

    A script that creates the table force_name on your MySQL server.

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,9 @@
+-- Create the table force_name
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
