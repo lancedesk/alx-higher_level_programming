692467e4a602873f4edf157f4cfc5867c72e6ec2
Lance Desk <lncedesk@gmail.com>
2024-01-13 05:27:44 -0800

    A script that lists all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -3,6 +3,7 @@
 Script that lists all State objects, and corresponding City objects,
 contained in the database hbtn_0e_101_usa.
 """
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
