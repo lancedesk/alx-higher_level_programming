f1e8de66f51dc58b3e48b2a62baf82d17d3845fa
Lance Desk <lncedesk@gmail.com>
2024-01-13 05:17:10 -0800

    A script that lists all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -4,44 +4,24 @@ Script that lists all State objects, and corresponding City objects,
 contained in the database hbtn_0e_101_usa.
 """
 
-from sys import argv
-from relationship_state import Base, State
-from relationship_city import City
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
 
-
-def list_states_and_cities(username, password, db_name):
-    """List all State objects and their corresponding City objects."""
+if __name__ == "__main__":
     # Create a SQLAlchemy engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(username, password, db_name),
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+
+    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # Query to get all State objects with their corresponding City objects
-    states_cities = session.query(State).order_by(State.id).all()
-
-    # Print the results
-    for state in states_cities:
+    for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
-
-    session.close()
-
-
-if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(argv) != 4:
-        print("Usage: {} <username> <password> <database>"
-              .format(argv[0]))
-        exit(1)
-
-    # Get the command-line arguments
-    username, password, db_name = argv[1], argv[2], argv[3]
-
-    # Call the function to list states and cities
-    list_states_and_cities(username, password, db_name)
