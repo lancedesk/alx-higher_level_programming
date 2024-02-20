259d9fe64b12c73a011894f1a998c5827620818b
Lance Desk <lncedesk@gmail.com>
2024-02-20 04:08:32 -0800

    A script that reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-const request = require('request');
 const fs = require('node:fs');
 const file_path = process.argv[2];
 
 fs.readFile(file_path, 'utf-8', (err, data) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	console.log(data);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
 });
