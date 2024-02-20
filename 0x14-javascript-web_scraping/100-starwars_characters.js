781035f739f243cd54afd68a497a82b77ea01780
Lance Desk <lncedesk@gmail.com>
2024-02-20 05:38:17 -0800

    A script that prints all characters of a Star Wars movie.

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,23 +4,22 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(url, function (error, response, body) {
-    if (error) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+
+  for (const character of data.characters) {
+    request(character, function (error, response, body) {
+      if (error) {
         console.error(error);
         return;
-    }
+      }
 
-    const data = JSON.parse(body);
-
-    for (const character of data.characters) {
-        request(character, function (error, response, body) {
-            if (error) {
-                console.error(error);
-                return;
-            }
-
-            const info = JSON.parse(body);
-
-            console.log(info.name);
-        });
-    }
+      const info = JSON.parse(body);
+      console.log(info.name);
+    });
+  }
 });
